d3f2af13ece54349774d62fd4e2ebf3d3a41cb68
duzunwu512 <94684873@qq.com>
2019-10-13 23:13:37 +0800

    date

---
+++ weixin/test/data/edu.js
@@ -0,0 +1,13 @@
+//获取url中的参数方法
+function getUrlParam(name){
+  //构造一个含有目标参数的正则表达式对象
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  //匹配目标参数
+  var r = window.location.search.substr(1).match(reg);
+  //返回参数
+  if (r != null) {
+      return unescape(r[2]);
+  } else{
+      return null;
+  }
+}
---
+++ weixin/test/data/gk.js
@@ -0,0 +1,18 @@
+var data_1 = {
+    title: 'waking up'
+    ,audio_url: '01-waking-up.mp3'
+    , result: [
+      { en: 'The rooster wakes up. Cock-a-doodle-doo!', s: '3', e: '8' },
+      { en: 'The cow wakes up. Moo, moo!', s: '8', e: '14' },
+      { en: 'The pig wakes up. Oink, oink!', s: '14', e: '20' },
+      { en: 'The horse wakes up. Neigh, neigh!', s: '20', e: '25' },
+      { en: 'The turkey wakes up. Gobble, gobble!', s: '25', e: '29' },
+      { en: 'The sheep wakes up. Baa, baa!', s: '29', e: '35' },
+      { en: 'The duck wakes up. Quack, quack!', s: '35', e: '40' },
+      { en: 'And the chick wakes up. Peep, peep!', s: '40', e: '45' }
+    ]
+  };
+
+  var db={
+    data_lv1_1: data_1,
+  };

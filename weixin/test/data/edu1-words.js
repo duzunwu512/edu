76e3d35d232805ee6b5fce35eb5ecbde0414ac05
duzunwu512 <94684873@qq.com>
2019-10-14 23:37:56 +0800

    'update'

---
+++ weixin/test/data/edu1-words.js
@@ -0,0 +1,35 @@
+var learn_tools = {
+    title: '学习用品'
+    , result: [
+      { en: 'pen', cn:'钢笔', pic:'' },
+      { en: 'pencil', cn:'铅笔' ,pic:''},
+      { en: 'pencil-case', cn:'铅笔盒' ,pic:''},
+      { en: 'ruler', cn:'尺子' ,pic:''},
+      { en: 'book', cn:'书' ,pic:''},
+      { en: 'bag', cn:'包' ,pic:''},
+      { en: 'post', cn:'明信片' ,pic:''}
+    ]
+  };
+  var body_parts = {
+    title: '身体部位'
+    , result: [
+      { en: 'foot', cn:'脚' ,pic:''},
+      { en: 'head', cn:'头' ,pic:''},
+      { en: 'face', cn:'脸' ,pic:''},
+      { en: 'hair', cn:'头发' ,pic:''},
+      { en: 'nose', cn:'鼻子' ,pic:''},
+      { en: 'mouth', cn:'嘴' ,pic:''},      
+      { en: 'eye', cn:'眼' ,pic:''},
+      { en: 'ear', cn:'耳朵' ,pic:''},      
+      { en: 'arm', cn:'手臂' ,pic:''},     
+      { en: 'hand', cn:'手' ,pic:''},
+      { en: 'finger', cn:'手指' ,pic:''},   
+      { en: 'leg', cn:'腿' ,pic:''},
+      { en: 'tail', cn:'尾巴' ,pic:''}
+    ]
+  };  
+
+  var db={
+    data_lv1_1: learn_tools,
+    data_lv1_2: body_parts,
+  };

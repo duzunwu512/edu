aa959003857fca65a6ee1c1347402e1b27ba55a9
gzldevelop <94684873@qq.com>
2019-10-18 16:07:06 +0800

    'update'

--- RuoYi/ruoyi-admin/src/main/java/com/ruoyi/WordGenerate.java
+++ RuoYi/ruoyi-admin/src/main/java/com/ruoyi/WordGenerate.java
@@ -15,7 +15,7 @@ import com.alibaba.fastjson.JSONObject;
 
 public class WordGenerate {
 	
-	
+	//catalog-words.txt
 	static Map<String, List<String>> readFile(String filename, List<String> list) {
 		File file = new File(filename);
 		Map<String, List<String>> subMap = null;
@@ -97,16 +97,95 @@ public class WordGenerate {
 			}
 			
 			System.out.println("json:::"+parent.toString());
-			
-			
 		}
+	}
+	
+	
+	//人教版新起点1-6年级单词表.txt
+	static Map<String, List<String>> readNewStartFile(String filename) {
+		File file = new File(filename);
+		Map<String, List<String>> subMap = null;
+		
+		if(file.exists()) {
+			BufferedReader br = null;
+			try {
+				br = new BufferedReader(new FileReader(file));
+				String line = null;
+				String title = null;
+				List<String> tmplist = null;
+				subMap = new HashMap<String, List<String>>();
+				
+				StringBuffer sb = new StringBuffer();
+				
+				String g = null;
+				String u = null;
+				String cn = null;
+				String en = null;
+				int grade = 0;
+				int unit = 0;
+				boolean nword = true;
+				boolean newu = true;
+				boolean newue=false;
+				while((line = br.readLine())!=null) {
+					if(line.startsWith("//")){//跳出第一行的 人教版新起点
+						continue;
+					}
+					line = line.trim();
+					if(line.startsWith("-")) {//如果是年级
+						grade++;
+						unit = 0;
+						g = line.trim().replace("-", "");
+						System.out.println("title::"+g);
+						
+					}else if(line.startsWith("=")) {
+						unit++;
+						newu = true;
+						if(newue){
+							sb.append("]\n").append("};\n");
+							newue = false;
+						}
+						u = line.trim().replace("=", "");
+						//System.out.println("title::"+u);
+						sb.append("var ").append("g").append(grade).append("_").append("u").append(unit).append(" = {\n")
+						.append("  title:'").append(u).append("',\n");
+						
+					}else {//如果是单词内容
+						line = line.trim();
+						cn = line.substring(line.lastIndexOf(" ")+1);
+						en = line.replace(cn, "").trim();
+						if(newu){
+							sb.append("  list:[");
+							newu = !newu;
+							newue = true;
+						}
+						sb.append("{en:'").append(en).append("', cn:'").append(cn).append("'},");
+					}
+					
+				}
+				sb.append("]\n").append("};\n");
+				System.out.println(""+sb.toString());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch(IOException ioe) {
+				ioe.printStackTrace();
+			}finally {
+				try {
+					if(br!=null)
+						br.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+		return subMap;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("sssssssssssss");
 		List<String> list = new ArrayList<String>();
-		Map<String, List<String>> data = readFile("G:\\project\\code\\RuoYi\\ruoyi-admin\\src\\main\\java\\com\\ruoyi\\catalog-words.txt", list);
-		writeJson(data, list);
+		/*Map<String, List<String>> data = readFile("G:\\project\\code\\RuoYi\\ruoyi-admin\\src\\main\\java\\com\\ruoyi\\catalog-words.txt", list);
+		writeJson(data, list);*/
+		
+		readNewStartFile("G:\\project\\code\\RuoYi\\ruoyi-admin\\src\\main\\resources\\data\\人教版新起点1-6年级单词表.txt");
 	}
 
 }
